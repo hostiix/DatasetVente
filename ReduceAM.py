b8905ad91e9569fa53bbcee98a9bfec18d515f79
SylvainCottet <silvain_1997@hotmail.fr>
2020-04-03 18:53:00 +0200

    Commit fichier mock données finalisés (Avec prise en compte des jours fériés) + Csv dates uniques entre 01/01/2015 et 31/12/2019

--- ReduceAM.py
+++ ReduceAM.py
@@ -14,8 +14,8 @@ file.write(
     '%s;%s;%s;%s;%s;%s;%s\n' % (
         "id_vente", "date_vente", "id_client", "id_magasin", "prix_vente_unitaire", "quantité", "id_produit"))
 
-jours_ferié_fermé = ["01/01", "01/05", "25/12"]
-jours_ferié_ouvert = ["08/05", "15/08", "14/07", "01/11", "11/11"]
+jours_ferié_fermé = ["1/1", "1/5", "25/12"]
+jours_ferié_ouvert = ["8/5", "15/8", "14/7", "1/11", "11/11"]
 
 # Nombre de produit dans la commande
 nb_product_invoice_random = random.randint(1, 10)
@@ -36,15 +36,24 @@ for line in sys.stdin:
     line = line.strip()
     line = line.split(";")
 
+    nb_vente_jours = 50000
+
     jour_de_la_semaine = line[0]
     annee = line[1]
     mois = int(line[2]) + 1
     mois = str(mois)
     jour_du_mois = line[3]
 
-    date = str(jour_du_mois) + "/" + mois + "/" + str(annee)
+    date_jours_mois = str(jour_du_mois) + "/" + mois
 
-    nb_vente_jours = 30000
+    if date_jours_mois in jours_ferié_fermé :
+        print("jours ferié fermé")
+        nb_vente_jours = nb_vente_jours *0
+    elif date_jours_mois in jours_ferié_ouvert :
+        print("jours ferié ouvert")
+        nb_vente_jours = nb_vente_jours *1.4
+
+    date = str(jour_du_mois) + "/" + mois + "/" + str(annee)
 
     #Lundi
     if jour_de_la_semaine == 1 :
@@ -102,66 +111,4 @@ for line in sys.stdin:
             '%s;%s;%s;%s;%s;%s;%s\n' % (
                 id_vente, date, id_client, id_magasin, prix, quantite, id_produit))
 
-
-"""
-
-# Initialisation d'un numéro d'id random, et d'une date
-id_invoice_temp = str(random.randint(0, 99))
-invoice_year = str(random.randint(2017,2019))
-invoice_month = str(random.randint(1,12))
-invoice_day = str(random.randint(1, 29))
-invoice_hour = str(random.randint(9, 22))
-invoice_minute = str(random.randint(0, 59))
-
-# Partitioner
-for line in sys.stdin:
-    # line = line.strip()
-    line = line.split(",")
-
-    # Vérification du nombre d'article dans la commande
-    if nb_product_invoice <= nb_product_invoice_random:
-        nb_product_invoice = nb_product_invoice + 1
-    else:
-        # Nouvelle commande -> Nouvel id, reinit du nb d'article, nouvelle date
-        id_invoice_temp = line[0]
-        id_invoice_random = str(random.randint(0, 10000))
-        nb_product_invoice = 1
-        invoice_year = line[2]
-        invoice_month = line[3]
-        invoice_day = str(random.randint(1, 29))
-        invoice_hour = str(random.randint(9, 22))
-        invoice_minute = str(random.randint(0, 59))
-
-    id_invoice = id_invoice_temp + id_invoice_random
-
-    quantity_line1 = int(line[1])
-
-    # Transformation sur les quantités, afin d'avoir des quantités cohérentes
-    if quantity_line1 > 10 and quantity_line1 <= 100:
-        quantity = quantity_line1 % 10
-    elif quantity_line1 > 100 and quantity_line1 <= 1000:
-        quantity = quantity_line1 % 100
-    elif quantity_line1 > 1000 and quantity_line1 <= 10000:
-        quantity = quantity_line1 % 1000
-    elif quantity_line1 > 1000 and quantity_line1 <= 100000:
-        quantity = quantity_line1 % 10000
-    elif quantity_line1 < 0:
-        quantity = random.randint(-7, -1)
-    else:
-        quantity = quantity_line1
-
-    invoice_date = invoice_day + "/" + invoice_month + "/" + invoice_year + " " + invoice_hour + ":" + invoice_minute + ":00"
-
-    product_unit_price_line4 = float(line[4])
-
-    if product_unit_price_line4 > 30 :
-        product_unit_price = round(random.uniform(0.8, 30), 3);
-    elif product_unit_price_line4 < 0:
-        product_unit_price = round(random.uniform(-2, 30),3);
-    else :
-        product_unit_price = product_unit_price_line4
-
-    file.write('%s;%s;%s;%s\n' % (
-        id_invoice, quantity, invoice_date, product_unit_price))
-"""
 file.close()
